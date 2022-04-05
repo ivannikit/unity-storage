581837332c785fa4c96d58c594c47169fe2b96cf
Ivan <ivannikit16@gmail.com>
2022-04-05 18:36:27 +0300

    add CacheContainer

---
+++ Runtime/Resource/Implementation/CacheContainer.cs
@@ -0,0 +1,33 @@
+#nullable enable
+
+namespace TeamZero.StorageSystem
+{
+    public class CacheContainer<TData> : IResource<TData>
+    {
+        private readonly IResource<TData> _source;
+        private TData _cache = default!;
+        private bool _cached = false;
+
+        public static CacheContainer<TData> Create(IResource<TData> source) 
+            => new CacheContainer<TData>(source);
+        
+        private CacheContainer(IResource<TData> source) => _source = source;
+
+        public bool Pull(out TData data)
+        {
+            if (!_cached)
+            {
+                if (_source.Pull(out TData sourceData))
+                {
+                    _cache = sourceData;
+                    _cached = true;
+                }
+            }
+
+            data = _cache;
+            return _cached;
+        }
+
+        public bool Push(TData data) => _source.Push(data);
+    }
+}
